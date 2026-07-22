180ef45fe600fca0293a30a167e337531208ae02
Siddhiii09 <siddhibande12@gmail.com>
2026-07-22 23:50:38 +0530

    Dashboard page added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import page from '../page'
-import Navbar from '@/components/Navbar'
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainActions from "@/components/dashboard/MainActions";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
+import Navbar from "@/components/Navbar";
 
-const DashboardPage = () => {
+function DashboardPage() {
   return (
-    <div>
+    <>
       <Navbar />
-      Dashboard
-    </div>
-  )
-}
 
-export default DashboardPage
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
+  );
+}
+export default DashboardPage;
